1e8abf6fc3bc6487a873cc191cf6acc0a7dc6eff
zgardner11 <zgardner.cinema@gmail.com>
2022-10-21 15:40:58 -0700

    cleaner functionality

--- Cottonwood-Vacation-Rental-main/Pages/JS/main.js
+++ Cottonwood-Vacation-Rental-main/Pages/JS/main.js
@@ -13,15 +13,15 @@ burger.addEventListener('click', () => {
 const formBook = document.getElementById('form-book');
 const userName = document.getElementById('name-book');
 const userEmail = document.getElementById('email-book');
-const date = document.getElementsByClassName('date-book');
-
+const dateStart = document.getElementById('date-start');  
+const dateEnd = document.getElementById('date-end');  
 
 //add listener for form to check inputs before submitting
 
 formBook.addEventListener("submit", e => {
   //prevents form from submitting
   e.preventDefault();
-
+  //check all inputs for empty string or type
   checkInputs();
 });
 
@@ -32,7 +32,8 @@ function checkInputs (){
 
   const usernameValue = userName.value.trim();
   const useremailValue = userEmail.value.trim();
-  const dateValue = date.value;
+  const dateStartValue = dateStart.value;
+  const dateEndValue = dateEnd.value;
 
   if(usernameValue === ''){
     // show error
@@ -52,23 +53,29 @@ function checkInputs (){
     setSuccess(userEmail);
   }
 
-  if(dateValue === ''){
+  if(!dateStartValue, !dateEndValue){
     // show error
     // add error class
-    setError(date, 'Please enter a valid email.'); 
+    console.log('no date value');
+    dateEmpty(dateStart, dateEnd); 
   } else{
     // add success class
-    setSuccess(date);
+    dateFilled(dateStart, dateEnd);
+    console.log('date start value');
   }
+
 };
 
 function setError(input, message) {
   const formEntry = input.parentElement; //declare variable for parent div of input (.form-entry)
   const small = formEntry.querySelector('small');
+
   //add error message inside small tag
   small.innerText = message;
+
   //add error class
   formEntry.className = 'form-entry error';
+
 };
 
 //add functionality so class automatically updates to success when user types in more than 2 characters for name and email is validated
@@ -82,3 +89,14 @@ function isEmail(email) {
   return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
+function dateEmpty(dateStart, dateEnd) {
+  dateStart.className = 'date-book error';  
+  dateEnd.className = 'date-book error';  
+  console.log('error');
+}
+
+function dateFilled(dateStart, dateEnd) {
+  dateStart.className = 'date-book success';  
+  dateEnd.className = 'date-book success';  
+  console.log('working');
+}
